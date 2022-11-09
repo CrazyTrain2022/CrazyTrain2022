3b9e79e5fb8e9b3e51b8f1dfe24f7dd09e6559f3
Lukas <lukjo147@student.liu.se>
2022-11-09 14:48:30 +0100

    added mocap helper to GUI and emergency may work as intended

--- GUI/gui_menu.py
+++ GUI/gui_menu.py
@@ -41,6 +41,7 @@ class Gui_menu:
         drone_submen.add_command(label="Select drones", command=self.select_drones)
         drone_submen.add_command(label="Start ROS server", command=self.start_ros_server)
         drone_submen.add_command(label="Start hover swarm", command=self.start_hover_swarm)
+        drone_submen.add_command(label="Start mocap helper", command=self.start_mocap_helper)
         drone_submen.add_command(label="Show drone position", command=self.drone_position)
         drone_submen.add_command(label="Update", command=self.update_drone_tabs)
         menu_widget.add_cascade(label="Drone", menu=drone_submen)
@@ -138,6 +139,10 @@ class Gui_menu:
     # output: -
     def start_hover_swarm(self):
         os.system('gnome-terminal -- bash GUI/bash_scripts/start_hover_swarm.sh')
+
+    def start_mocap_helper(self):
+        os.system('gnome-terminal -- bash GUI/bash_scripts/start_mocap_helper.sh')
+
 
 
     # runs a bash script that will provide the drone position
