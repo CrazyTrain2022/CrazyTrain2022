f1dd5b3972fd77d488c91f3dd858f592c3349cf6
Oskar Grönlund <oskar@DESKTOP-6N3C93C.localdomain>
2022-12-06 11:08:19 +0100

    Testing i RRT*

--- GUI/run_rrt.py
+++ GUI/run_rrt.py
@@ -17,10 +17,36 @@ def run_planner(drone_id):
     VISIONEN_Y_DIM = 8
     VISIONEN_Z_DIM = 3.0
 
-    # Create the surronding world as an object BoxWorld
-    world = BoxWorld([[-VISIONEN_X_DIM/2, VISIONEN_X_DIM/2], [-VISIONEN_Y_DIM/2, VISIONEN_Y_DIM/2], [0, VISIONEN_Z_DIM]])
+    # Constraint the world around set-points and obstacles
+    CALC_X_MAX = 0
+    CALC_X_MIN = 0
+    CALC_Y_MAX = 0
+    CALC_Y_MIN = 0
+    CALC_Z_MAX = 0
+    CALC_Z_MIN = 0 # Always 0
 
-    # Coordinates are closest to origo h,w,d is the box dim
+    # Read drone?waypoints.csv file
+    #with open('../GUI/points_csv/drone'+str(drone_number)+'waypoints.csv', 'r') as file:
+    with open('GUI/points_csv/drone'+drone_id+'waypoints.csv', 'r') as file:
+        reader = csv.reader(file, skipinitialspace=True)
+        points = np.empty((0,3),int)
+        for coord in reader:
+            step = np.array([[float(coord[0]),float(coord[1]),float(coord[2])]])
+            points = np.append(points ,step, axis = 0)
+            # Constraining the world coordinates
+            if step[0] <= CALC_X_MIN:
+                CALC_X_MIN = step[0] - 1
+            if step[1] <= CALC_Y_MIN:
+                CALC_Y_MIN = step[1] - 1
+            if step[1] >= CALC_X_MAX:
+                CALC_X_MAX = step[0] + 1
+            if step[1] >= CALC_Y_MAX:
+                CALC_Y_MAX = step[1] + 1
+            if step[2] >= CALC_Z_MAX:
+                CALC_Z_MAX = step[5] + 1
+        print("Waypoints csv-file read!")
+
+    # Read obstacles.csv file
     #sys.path.append("/home/crazycrowd/CrazyTrain/CrazyTrain2022/crazyswarm/ros_ws/src/crazyswarm/scripts/pycrazyswarm")
     file_obs = "crazyswarm/ros_ws/src/crazyswarm/scripts/pycrazyswarm/visualizer/obstacles.csv"
     with open(file_obs, 'r') as file:
@@ -29,27 +55,31 @@ def run_planner(drone_id):
         for coord in reader:
             step = np.array([[float(coord[0]),float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4]),float(coord[5])]])
             obs = np.append(obs ,step, axis = 0)
-        for i in range(0,len(obs)):
-            world.add_box(obs[i][0],obs[i][1],obs[i][2],obs[i][3],obs[i][4],obs[i][5])
+            # Constraining the world coordinates
+            if step[0] <= CALC_X_MIN:
+                CALC_X_MIN = step[0] - 1
+            if step[1] <= CALC_Y_MIN:
+                CALC_Y_MIN = step[1] - 1
+            if step[3] >= CALC_X_MAX:
+                CALC_X_MAX = step[0] + 1
+            if step[4] >= CALC_Y_MAX:
+                CALC_Y_MAX = step[1] + 1
+            if step[5] >= CALC_Z_MAX:
+                CALC_Z_MAX = step[5] + 1
+        print("Obstacles csv-file read!")
 
-    # Define planner options
-    opts = {
-        "beta": 0.8,  # Probability of selecting goal state as target state
-        "lambda": 0.1,  # Step size
-        "eps": 0.01,  # Threshold for stopping the search (negative for full search)
-        "r_neighbor": 0.5,  # Radius of circle for definition of neighborhood
-        "K": 10000, # Maximum number of iterations 
-    }  
-
-    # Read .csv file
-    #with open('../GUI/points_csv/drone'+str(drone_number)+'waypoints.csv', 'r') as file:
-    with open('GUI/points_csv/drone'+drone_id+'waypoints.csv', 'r') as file:
-        reader = csv.reader(file, skipinitialspace=True)
-        points = np.empty((0,3),int)
-        for coord in reader:
-            step = np.array([[float(coord[0]),float(coord[1]),float(coord[2])]])
-            points = np.append(points ,step, axis = 0)
-        print("File Read")
+    # Create the surronding world as an object BoxWorld and add obstacles
+    #world = BoxWorld([[-VISIONEN_X_DIM/2, VISIONEN_X_DIM/2], [-VISIONEN_Y_DIM/2, VISIONEN_Y_DIM/2], [0, VISIONEN_Z_DIM]])
+    world = BoxWorld([[CALC_X_MIN, CALC_X_MAX], [CALC_Y_MIN, CALC_Y_MAX], [CALC_Z_MIN, CALC_Z_MAX]])
+    for i in range(0,len(obs)):
+        world.add_box(obs[i][0],obs[i][1],obs[i][2],obs[i][3],obs[i][4],obs[i][5])
+    
+    print("World x-min: ",CALC_X_MIN)
+    print("World y-min: ",CALC_Y_MIN)
+    print("World z-min: ",CALC_Z_MIN)
+    print("World x-max: ",CALC_X_MAX)
+    print("World y-max: ",CALC_Y_MAX)
+    print("World z-max: ",CALC_Z_MAX)
 
     # Run the planner
     path = np.vstack(points[0])
